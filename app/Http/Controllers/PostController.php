9145e1fa10b49dcc2a947429775592a80641c9d1
Nikolai <ornichola@users.noreply.github.com>
2017-05-20 22:04:31 +0300

    Adding Scope
    Adding Scope functional to the Post model.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,15 +11,17 @@ class PostController extends Controller
     // Создано через консоль, используя команду php artisan make:controller PostController
     // Однако методы почему-то не создались автоматически. Будут созданы вручную
     
-    public function index()
+    public function index(Post $postModel)
     {
         //$posts = Post::all();
         ////dd($posts);
         //$posts = Post::latest('id')->get();
         //$posts = Post::latest('published_at')->get();
-        $posts = Post::latest('published_at')
-                ->where('published_at', '<=', Carbon::now())
-                ->get();
+//        $posts = Post::latest('published_at')
+//                ->where('published_at', '<=', Carbon::now())
+//                ->get();
+        
+        $posts = $postModel->getPublishedPosts();
         return view('post.index', ['posts' => $posts]);
         //echo 'index';
     }	
